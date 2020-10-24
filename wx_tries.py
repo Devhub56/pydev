358f2cd04916ccfbc0cb3742d5a757459be1f94e
DevHub <56557924+Devhub56@users.noreply.github.com>
2020-10-24 16:16:13 +0200

    Update wx_tries.py

--- wx_tries.py
+++ wx_tries.py
@@ -2,6 +2,7 @@
 # nominimizebox.py
 import wx
 app = wx.App()
+#can remove the property we don't want the window to haveby not including it in the style
 window = wx.Frame(None, style=wx.MINIMIZE_BOX | wx.MAXIMIZE_BOX | wx.RESIZE_BORDER
 | wx.SYSTEM_MENU | wx.CAPTION | wx.CLOSE_BOX)
 window.Show(True)
