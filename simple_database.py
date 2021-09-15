158ef4ff34dcebc82109da3753f6c68c00ebc834
DevHub <56557924+Devhub56@users.noreply.github.com>
2021-09-15 12:57:09 +0200

    Update simple_database.py

--- simple_database.py
+++ simple_database.py
@@ -3,7 +3,7 @@
 import sys, shelve
 name = input('Enter your name : ') or '<unknown>'
 pwd = input('What is your password : ')
-database= [['fel','6140'],['superuser','root']]
+database= [['some_guy','6140'],['superuser','root']]
 if [name.lower(),pwd] in database: print("welcome ,"+ name+ "," + "How can we help you today ?" )
 else:
     print('Acess Denied !!')
