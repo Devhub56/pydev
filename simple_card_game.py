a64f6c8a28499e66b7cdf3dccc84491d9229e51e
DevHub <56557924+Devhub56@users.noreply.github.com>
2021-11-03 21:03:22 +0200

    Update simple_card_game.py

--- simple_card_game.py
+++ simple_card_game.py
@@ -9,7 +9,7 @@ num   = int(input('How many dice? '))
 sides = int(input('How many sides per die? '))
 sum = 0
 for i in range(num): sum += gamble(sides) + 1
-print('The result is', sum)
+print(f'The result is {sum}')
 
 #shuffling cards
 values = list(range(1, 11)) + 'Jack Queen King'.split()
@@ -17,6 +17,6 @@ suits = 'diamonds clubs hearts spades'.split()
 deck = ['{} of {}'.format(v, s) for v in values for s in suits]
 shuffle(deck)
 pprint(deck[:12])
-#dealing a card whenever the Enter kry is pressed
+#dealing a card whenever the Enter key is pressed
 while deck:
     input(deck.pop())
