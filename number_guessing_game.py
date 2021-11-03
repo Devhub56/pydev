e8dbb651d3e63a80c3148371b03f6f7e8c4311c2
DevHub <56557924+Devhub56@users.noreply.github.com>
2021-11-03 17:38:19 +0200

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,6 +1,6 @@
 import random
 
-def guess (x):
+def guess_number (x):
     number = random.randint(1,x)
     user_guess = 0
     
@@ -12,20 +12,21 @@ def guess (x):
              print (f"You entered {user_guess} , which is lower than the number, please try again with a bigger number")
     print (f"Yay, you guessed {number} correctly..")
     
-guess(100)
+guess_number(100)
+
 ### Now time for the computer to guess the user's secret  number :
 
 print ("Now Think of a number Between 1 and 30 and let let the computer guess your number")#can be modified to meet any limit arguments when calling the function below
 def computer_guess (y):
     low = 1
     high = y
-    feedback = ""
-    while feedback != "c":
+    user_feedback = ""
+    while user_feedback != "c":
         random_number = random.randint(low,high)
-        feedback =input(f'Is {random_number} high (H),Low (L) or Correct (C)?').lower() # takes care of any upper case entry
-        if feedback == 'h':
+        user_feedback =input(f'Is {random_number} high (H),Low (L) or Correct (C)?').lower() # takes care of any upper case entry
+        if user_feedback == 'h':
             high = random_number-1
-        if feedback == 'l':
+        if user_feedback == 'l':
              low = random_number + 1
     print(f'Yay! The computer correctly guessed {random_number}.Which was your secret number!')
 computer_guess (30)
