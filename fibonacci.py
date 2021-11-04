7b74a8040fc7d9f9bc850bb20da7d52f851c5664
DevHub <56557924+Devhub56@users.noreply.github.com>
2021-11-04 17:29:45 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,15 @@
 
-memo = {}
+memo = {} # creating a dictionary to store our fibs helps us re-use some of the values from our earlier iterations 
 
 def fibs(n):
     if n in memo:
-        return memo[n]
+        return memo[n] 
     if n <= 2:
         f = 1 
     else :
         f = fibs (n-1)+ fibs (n-2)
     memo[n] = f
     return f
-    
+
+#test case    
 print (fibs(10))
