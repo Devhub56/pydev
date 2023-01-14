c3336cb9129187a9a58a24aaf95694f45805c54f
DevHub <56557924+Devhub56@users.noreply.github.com>
2023-01-14 13:46:11 +0200

    Update flask_app.py

--- pywebapp/login/flask_app.py
+++ pywebapp/login/flask_app.py
@@ -70,6 +70,6 @@ def register():
 	elif request.method == 'POST': 
 		msg = 'Please fill out the form !'
 	return render_template('register.html', msg = msg) 
-
+if __name__ == '__main__':
 app.config["DEBUG"]=True
 app.run()
